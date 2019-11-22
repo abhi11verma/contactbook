a55036b2a5edaf96a0945caf197b861cb08a898f
Abhishek Verma <abhi11verma@gmail.com>
2019-11-22 11:02:35 +0530

    Basic Application initialization and running.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className="App-title">Contact Book</h2>
+      <div>Form</div>
+      <div>List</div>
     </div>
   );
 }
